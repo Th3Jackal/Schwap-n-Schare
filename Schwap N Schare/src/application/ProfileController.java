da1ab9ec7227f95fc4d760f56c54c11ea49d42c6
Daniel Albasini <99764455+Th3Jackal@users.noreply.github.com>
2022-04-24 23:38:29 -0500

    Add files via upload

--- Schwap N Schare/src/application/ProfileController.java
+++ Schwap N Schare/src/application/ProfileController.java
@@ -1,21 +1,117 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+//import java.util.Arrays;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class ProfileController {
-//initialized profile page variables
-//function declared to for logout feature 
+	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
+	@FXML
+	private Label name;
+	@FXML
+	private Label username;
+	@FXML
+	public TextArea bio;
+	@FXML
+	public TextArea classes;
+	
+	//Lists all of the relevant profile data on initialization of the scene
+	@FXML
+	public void initialize() {
+		
+		//This makes the text areas to neither scroll horizontally nor be edited
+		bio.setEditable(false);
+		classes.setEditable(false);
+		bio.setWrapText(true);
+		classes.setWrapText(true);
+		
+		//This loads the name and username from name.txt to display them
+		File file = new File(System.getProperty("user.dir") + "/src/" +  "name.txt");
+		try {
+			FileReader reader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			
+			String line;
+
+			int i = 1;
+			
+			while ((line = bufferedReader.readLine()) != null) {
+				if (i == 1) {
+					name.setText(line);
+				}
+				else if (i == 2) {
+					username.setText(line);
+				}
+				i++;
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			name.setText("Name");
+			username.setText("Username");
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+		
+		//This loads the biography from bio.txt to display it
+		File file2 = new File(System.getProperty("user.dir") + "/src/" +  "bio.txt");
+		try {
+			FileReader reader = new FileReader(file2);
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			
+			String line;
+			
+			while ((line = bufferedReader.readLine()) != null) {
+				bio.appendText(line);
+				bio.appendText("\r\n");
+			}
+			
+			reader.close();
+		} catch (FileNotFoundException e) {
+			bio.setText("");
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+		
+		//This loads the class list from classes.txt to display it
+		File file3 = new File(System.getProperty("user.dir") + "/src/" +  "classes.txt");
+		try {
+			FileReader reader = new FileReader(file3);
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			
+			String line;
+			
+			while ((line = bufferedReader.readLine()) != null) {
+				classes.appendText(line);
+				classes.appendText("\r\n");
+			}
+
+			reader.close();
+		} catch (FileNotFoundException e) {
+			classes.setText("");
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+		
+	}
+	
+	//This enables switching back to the menu from the profile
 	public void switchToMain(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -24,6 +120,7 @@ public class ProfileController {
 		stage.show();
 	}
 	
+	//This allows the user to close the app when clicking the logout button
 	public void Logout(ActionEvent event) throws IOException {
 		System.exit(0);
 	}
--- Schwap N Schare/src/application/SettingsController.java
+++ Schwap N Schare/src/application/SettingsController.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SettingsController {
@@ -15,6 +20,22 @@ public class SettingsController {
 	private Scene scene;
 	private Parent root;
 	
+	@FXML
+	private TextField textField1;
+	@FXML
+	private TextField textField2;
+	@FXML
+	private Button myButton;
+	@FXML
+	private Button myButton2;
+	@FXML
+	private Button myButton3;
+	@FXML
+	private TextArea textArea1;
+	@FXML
+	private TextArea textArea2;
+	
+	//This enables the user to move back to the app's menu
 	public void switchToMain(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -23,8 +44,57 @@ public class SettingsController {
 		stage.show();
 	}
 	
-	public void Save(ActionEvent event) throws IOException {
-		
+	//This saves the name and username to name.txt
+	public void save(ActionEvent event) {
+	    try {
+	    	File file = new File(System.getProperty("user.dir") + "/src/" +  "name.txt");
+	    	FileWriter myWriter = new FileWriter(file);
+	    	
+	    	String name = textField1.getText();
+	    	String username = textField2.getText();
+	    	
+	    	myWriter.write(name);
+	    	myWriter.write("\r\n");
+	    	myWriter.write(username);
+	    	
+	    	myWriter.close();
+	    } catch (IOException e) {
+	    	System.out.println(e);
+	    	e.printStackTrace();
+	    }
 	}
-
+	
+	//This saves the entered biography to bio.txt
+	public void saveBio(ActionEvent event) {
+	    try {
+	    	File file = new File(System.getProperty("user.dir") + "/src/" +  "bio.txt");
+	    	FileWriter myWriter = new FileWriter(file);
+	    	
+	    	String bio = textArea1.getText();
+	    	
+	    	myWriter.write(bio);;
+	    	
+	    	myWriter.close();
+	    } catch (IOException e) {
+	    	System.out.println(e);
+	    	e.printStackTrace();
+	    }
+	}
+	
+	//This saves the entered class list to classes.txt
+	public void saveClass(ActionEvent event) {
+	    try {
+	    	File file = new File(System.getProperty("user.dir") + "/src/" +  "classes.txt");
+	    	FileWriter myWriter = new FileWriter(file);
+	    	
+	    	String classes = textArea2.getText();
+	    	
+	    	myWriter.write(classes);
+	    	
+	    	myWriter.close();
+	    } catch (IOException e) {
+	    	System.out.println(e);
+	    	e.printStackTrace();
+	    }
+	}
 }
