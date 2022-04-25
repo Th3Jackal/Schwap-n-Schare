82edfd9ce06d0d2321cc05d9beb42c040363b414
rjvareed <99996854+rjvareed@users.noreply.github.com>
2022-04-24 19:00:57 -0500

    Add files via upload

--- Schwap N Schare/src/application/ViewScheduleController.java
+++ Schwap N Schare/src/application/ViewScheduleController.java
@@ -34,29 +34,33 @@ public class ViewScheduleController {
 		Schedule schedule = new Schedule();
 		schedule.addEvent("Assignment 4", 3, 16, 12, 5, "NPB 1.28");
 		schedule.addEvent("Test", 6, 23, 59, 59, "Times Square");
+		schedule.addEvent("Test", 6, 0, 0, 0, "Square");
 		schedule.addEvent("Group Project", 0, 5, 00, 00, "Mckinley Hall");
 		schedule.addEvent("Counselor appointment", 5, 13, 15, 00, "NPB 1.28");
 		
-		
-		double width = gc.getCanvas().getWidth();
-		double height = gc.getCanvas().getHeight();
-		double columnWidth = width/7;
+		//width 577
+		//height 304
+		//column width 82.4285714286
+		//text height 13
 		gc.setFill(Color.WHITE);
-		gc.fillRect(0, 0, width, height);
+		gc.fillRect(0, 0, 577.0, 304.0);
 		gc.setFill(Color.BLACK);
-		gc.setFont(Font.font("Sans Serif", columnWidth*.12));
-		gc.strokeLine(0,columnWidth*.27,width,columnWidth*.27);
+		gc.setFont(Font.font("Sans Serif", 9.89142857143));
+		gc.strokeLine(0,22.2557142857,577.0,22.2557142857);
 		for(int i=0; i<7; i++) {
-			gc.strokeLine(columnWidth*i,0, columnWidth*i, height);
-			gc.setFont(Font.font("Sans Serif", columnWidth*.15));
-			gc.fillText(ScheduleDate.days[i], 2+i*columnWidth, columnWidth*.19);
+			gc.strokeLine(82.42857142857143*i,0, 82.42857142857143*i, 304.0);
+			gc.setFont(Font.font("Sans Serif", 12.3642857143));
+			gc.fillText(ScheduleDate.days[i], 2+i*82.42857142857143, 82.42857142857143*.19);
+			gc.setFont(Font.font("Sans Serif", 9.89142857143));
 			for(ScheduleEvent se : schedule.getEvents(i)) {
-				double eheight = columnWidth*.18+(height - columnWidth*.19)*(double)(se.getDate().getRealTime())/86400.0;
-				gc.fillText(se.getLocation(), 2+i*columnWidth, eheight);
+				double eheight = 82.42857142857143*.39+236.338571429*(double)(se.getDate().getRealTime())/86400.0;
+				gc.fillText(se.getName(), 2+i*82.42857142857143, eheight);
+				gc.fillText(se.getDate().getTime(), 2+i*82.42857142857143, eheight+13);
+				gc.fillText(se.getLocation(), 2+i*82.42857142857143, eheight+26);
 			}
 		}
 		
-}
+	}
 	
 	
 public void switchToMain(ActionEvent event) throws IOException {
